15c929b81ffd48f3b78596b9b9cd30529cfde830
Joshua McFarland <mcfarljw@gmail.com>
2017-06-04 10:41:44 +0800

    Minor format updates

--- www/firebase-admob.js
+++ www/firebase-admob.js
@@ -1,37 +1,37 @@
 var exec = require('cordova/exec');
 
 module.exports = {
-  addTestDevice: function(deviceId) {
+  addTestDevice: function (deviceId) {
     return new Promise(
-      function(resolve, reject) {
+      function (resolve, reject) {
         exec(resolve, reject, 'FirebaseAdmobPlugin', 'addTestDevice', [deviceId]);
       }
     );
   },
-  getTestDevices: function() {
+  getTestDevices: function () {
     return new Promise(
-      function(resolve, reject) {
+      function (resolve, reject) {
         exec(resolve, reject, 'FirebaseAdmobPlugin', 'getTestDevices', []);
       }
     );
   },
-  requestInterstitial: function() {
+  requestInterstitial: function () {
     return new Promise(
-      function(resolve, reject) {
+      function (resolve, reject) {
         exec(resolve, reject, 'FirebaseAdmobPlugin', 'requestInterstitial', []);
       }
     );
   },
-  setInterstitialAdUnitId: function(adUnitId) {
+  setInterstitialAdUnitId: function (adUnitId) {
     return new Promise(
       function(resolve, reject) {
         exec(resolve, reject, 'FirebaseAdmobPlugin', 'setInterstitialAdUnitId', [adUnitId]);
       }
     );
   },
-  showInterstitial: function() {
+  showInterstitial: function () {
     return new Promise(
-      function(resolve, reject) {
+      function (resolve, reject) {
         exec(resolve, reject, 'FirebaseAdmobPlugin', 'showInterstitial', []);
       }
     );
