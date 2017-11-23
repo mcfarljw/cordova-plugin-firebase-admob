4c79968d023b1b27078aed783e46c156df58d6ed
Joshua McFarland <4167341+mcfarljw@users.noreply.github.com>
2017-11-23 16:41:42 +0800

    Return fake success for all ios functions

--- src/ios/FirebaseAdmobPlugin.m
+++ src/ios/FirebaseAdmobPlugin.m
@@ -6,27 +6,47 @@
 
 - (void)addTestDevice:(CDVInvokedUrlCommand*)command
 {
+  CDVPluginResult* pluginResult = nil;
 
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)getTestDevices:(CDVInvokedUrlCommand*)command
 {
+  CDVPluginResult* pluginResult = nil;
 
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)requestInterstitial:(CDVInvokedUrlCommand*)command
 {
+  CDVPluginResult* pluginResult = nil;
 
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)setInterstitialAdUnitId:(CDVInvokedUrlCommand*)command
 {
+  CDVPluginResult* pluginResult = nil;
 
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 - (void)showInterstitial:(CDVInvokedUrlCommand*)command
 {
+  CDVPluginResult* pluginResult = nil;
 
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+
+  [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
